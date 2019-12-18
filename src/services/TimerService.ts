c38c46d8de72402c3cebc51e969f2dad52ad4d14
GSE995 <gorynovse@gmail.com>
2019-12-18 23:51:51 +0300

    added TimerService

---
+++ src/services/TimerService.ts
@@ -0,0 +1,77 @@
+import Timer from '../models/Timer'
+import {Result, SuccessResult, ErrorResult} from '../models/Result'
+import ListResult from '../models/ListResult'
+import PageSize from '../models/PageSize'
+import firebase from '../config/firebase'
+
+const db = firebase.firestore()
+const collection = db.collection('timers')
+
+const API_ROOT_URL = '/api/timer'
+export { API_ROOT_URL }
+
+export default class TimersServie<T> {
+
+    static async create(timer: Timer): Promise<SuccessResult<Timer> | ErrorResult> {
+        try {
+            let timerForSave = {
+                name: timer.name,
+                endDate: timer.endDate || null,
+                startDate: timer.startDate
+            }
+            let docRef: any = await collection.add(timerForSave)
+            let serverTimer = new Timer(timer.name)
+            serverTimer.startDate = timer.startDate
+            serverTimer.id = docRef.id
+            return new SuccessResult(serverTimer)
+        } catch (error) {
+            console.log(error)
+            return new ErrorResult(error)
+        }
+    }
+
+    static async save(timer: Timer): Promise<SuccessResult<Timer> | ErrorResult> {
+        try {
+            let timerForSave = {
+                name: timer.name,
+                endDate: timer.endDate || null,
+                startDate: timer.startDate
+            }
+            let res = await collection.doc(timer.id).update(timerForSave)
+            return new SuccessResult(timer)
+        } catch (error) {
+            console.log(error)
+            return new ErrorResult(error)
+        }
+    }
+
+    static async remove(id: string): Promise<SuccessResult<Boolean> | ErrorResult> {
+        try {
+            let response = await collection.doc(id).delete()
+            return new SuccessResult(true)
+        } catch (error) {
+            console.log(error)
+            return new ErrorResult(error)
+        }
+    }
+
+    static async getList(pageSize: PageSize): Promise<SuccessResult<ListResult<Timer>> | ErrorResult> {
+        try {
+            let querySnapshot = await collection.orderBy('endDate', 'desc') .get()
+            let timers: any = []
+            
+            querySnapshot.forEach(doc => {
+                let data = doc.data()
+                let timer = new Timer(data.name)
+                timer.id = doc.id
+                timer.startDate = data.startDate.toDate()
+                timer.endDate = data.endDate ? data.endDate.toDate() : null
+                timers.push(timer)
+            })
+            return new SuccessResult(new ListResult<Timer>(timers, 1, pageSize))
+        } catch (error) {
+            console.log(error)
+            return new ErrorResult(error)
+        }
+    }
+}
