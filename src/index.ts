cdff6aab09a7220dd4eb08c0c14e8c89499f7831
GSE995 <gorynovse@gmail.com>
2019-12-27 23:37:22 +0300

    added simple cors config

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,21 @@
 import * as express from 'express'
-import * as parser from 'body-parser'
-import * as cors from 'cors'
 import * as bodyParser from 'body-parser'
 import * as morgan from 'morgan'
 
+import UseCors from './config/cors'
 import AddControllers from './config/controllers'
 
-const BudgetManagerPORT = process.env.PORT || 3001;
+const port = process.env.PORT || 3001;
 
 const app = express();
-
 app.use(express.static('.'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan('combined'))
 app.use(bodyParser.json())
-app.use(cors)
 
+UseCors(app)
 AddControllers(app)
 
-app.listen(BudgetManagerPORT, () => {
-	console.log(`Budget manager api running on ${BudgetManagerPORT}`)
+app.listen(port, () => {
+	console.log(`API running on ${port}`)
 })
